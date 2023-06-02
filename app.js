73ed838e487730e46fb46f283e40d6318a39c012
omarelsabaawy <omarelsabaawy77@gmail.com>
2023-06-02 10:27:24 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use('/', (req, res, next) => {
+    res.send("HERE'S PNC WEBSITE RUNNING.")
+});
+
+const password = 'pnc123456789';
+
+const MONGO_URI = "mongodb+srv://PNC-EG:pnc123456789@cluster0.9ndqb3n.mongodb.net/pnc-test"
+
+mongoose
+    .connect(MONGO_URI)
+    .then(result => {
+        console.log('Connect...');
+        app.listen(8000);
+    })
+    .catch(err => {
+        console.log(err);
+    });
